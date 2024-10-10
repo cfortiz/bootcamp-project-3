4ae5d04b9bd159735c9a88af4184418cc954581e
elizabethlconn <148993813+elizabethlconn@users.noreply.github.com>
2024-10-10 16:48:23 -0400

    Add filter dropdown

--- streamlit-test2.py
+++ streamlit-test2.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from pymongo import MongoClient
 import plotly.express as px
+import folium
+from streamlit_folium import st_folium
+# from geopy.geocoders import Nominatim
+# import time
+
 
 # mongo = MongoClient(port=27017)
 def get_mongo_client():
@@ -21,6 +26,21 @@ def get_data_from_mongodb():
 
     return data
 
+# ######################### Coordinates ####################
+# def get_coordinates(country_name):
+#     geolocator = Nominatim(user_agent="geoapiExercises", timeout=10)
+#     try:
+#         location=geolocator.geocode(country_name)
+#         time.sleep(1) # delay to avoid hitting API rate limits
+#         if location:
+#             return location.latitude, location.longitude
+#         else:
+#             return None, None
+#     except Exception as e:
+#         st.write(f"Error fetching coordinates for {country_name}: {e}")
+#         return None, None
+######################### Coordinates ####################
+
 # Convert Mongo data into a pandas DataFrame
 def mongo_data_to_dataframe(data):
     df = pd.DataFrame(data)
@@ -33,6 +53,11 @@ def mongo_data_to_dataframe(data):
     df['year'] = df['year'].astype(int)
 
     return df
+
+# Read country_coordinates CSV
+def load_country_coordinates(csv_file):
+    file_path = f'back-end/{csv_file}'
+    return pd.read_csv(file_path)
 
 # Streamlit app
 def main():
@@ -50,6 +75,23 @@ def main():
         # Filter out rows with no data
         df = df.dropna(subset=['Life Ladder', 'Country name'])
 
+        # Load coordinates from CSV file
+        coordinates_df = load_country_coordinates('country_coordinates.csv')
+
+        # Merge mongo db with the coordinates_df
+        df = pd.merge(df, coordinates_df, on='Country name', how='left')
+
+        # Filter out if missing geocoordinates
+        df = df.dropna(subset=['latitude', 'longitude'])
+
+######################### Coordinates ####################
+        # Fetch latitude and longitude for each country and ass as new columns
+        # st.write("Fetching coordinates for countries, please wait...")
+        # df['latitude'], df['longitude'] = zip(*df['Country name'].apply(get_coordinates))
+
+        # # Filter out rows if geocodeing fails
+        # df = df.dropna(subset=['latitude', 'longitude'])
+######################### Coordinates ####################
         # create choropleth map
         fig = px.choropleth(df,
                             locations='Country name',
@@ -62,8 +104,19 @@ def main():
         st.title('World Happiness Map')
         st.plotly_chart(fig)
 
-        # Display the data as a table in Streamlit
-        st.dataframe(df)
+        # Create Folium map with markers
+        m = folium.Map(location=[20,0], zoom_start=2)
+
+        # Add markers for each country
+        for i, row in df.iterrows():
+            folium.Marker(
+                location=[row['latitude'], row['longitude']],
+                popup=f"{row['Country name']} - Happiness Score: {row['Life Ladder']}"
+            ).add_to(m)
+        
+        # Display folium map with Streamlit
+        st_folium(m, width=1000, height=600)
+        
 
 if __name__ == "__main__":
     main()
