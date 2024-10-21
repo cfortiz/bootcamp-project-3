f0446942e3e679e8a27ceed21898fda3d59e4276
Carlos F Ortiz <bitsplit@gmail.com>
2024-10-21 01:13:51 -0400

    Refactor importdata.py update docstrings

--- importdata.py
+++ importdata.py
@@ -11,7 +11,20 @@ TABLE_DATA_FILENAME = 'back_end/resources/world-happiness-table-2024.csv'
 mongo = MongoClient(port=27017)
 
 
+def load_csv_into_mongo(filename, db_name, collection_name):
+    """Load data from a CSV file into a mongo collection."""
+    clear_mongo_collection(db_name, collection_name)
+    insert_into_mongo(load_csv_data(filename), db_name, collection_name)
+
+
 def load_csv_data(filename):
+    """Load data from a CSV file into a list of dicts.
+    
+    Insures that numeric fields are converted to numeric types as needed.  In
+    particular, it converts 'year' field values to `int` and other non-text
+    fields to `float`.
+    
+    """
     with open(filename, 'r', encoding='utf-8-sig') as fig_data_file:
         reader = csv.reader(fig_data_file)
         raw_data = list(reader)
@@ -36,17 +49,24 @@ def load_csv_data(filename):
     return parsed_data
 
 
+def clear_mongo_collection(db_name, collection_name):
+    """Clear out a mongo collection."""
+    db = mongo[db_name]
+    collection = db[collection_name]
+    collection.delete_many({})
+
+
 def insert_into_mongo(data, db_name, collection_name):
+    """Insert data into a mongo collection."""
     db = mongo[db_name]
     collection = db[collection_name]
     collection.insert_many(data)
 
 
 def import_data():
-    insert_into_mongo(load_csv_data(FIG_DATA_FILENAME), DB_NAME,
-                      FIG_COLLECTION_NAME)
-    insert_into_mongo(load_csv_data(TABLE_DATA_FILENAME), DB_NAME,
-                      TABLE_COLLECTION_NAME)
+    """Import CSV files into our mongo database collections."""
+    load_csv_into_mongo(FIG_DATA_FILENAME, DB_NAME, FIG_COLLECTION_NAME)
+    load_csv_into_mongo(TABLE_DATA_FILENAME, DB_NAME, TABLE_COLLECTION_NAME)
 
 
 if __name__ == '__main__':
